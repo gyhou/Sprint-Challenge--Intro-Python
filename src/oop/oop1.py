877546c4e2282c99ef8f25399e679e72dc30dc64
gyhou <47550192+gyhou@users.noreply.github.com>
2019-12-06 10:33:28 -0800

    Add files via upload

--- src/oop/oop1.py
+++ src/oop/oop1.py
@@ -17,3 +17,44 @@
 #     pass
 #
 # Put a comment noting which class is the base class
+
+
+class Vehicle:
+    # Parent class
+    pass
+
+
+class FlightVehicle(Vehicle):
+    # Child class - FlightVehicle
+    # Parent class - Vechile
+    pass
+
+
+class Starship(FlightVehicle):
+    # Child class - Starship
+    # Parent class - FlightVehicle
+    pass
+
+
+class Airplane(FlightVehicle):
+    # Child class - Starship
+    # Parent class - FlightVehicle
+    pass
+
+
+class GroundVehicle(Vehicle):
+    # Child class - GroundVehicle
+    # Parent class - Vechile
+    pass
+
+
+class Car(GroundVehicle):
+    # Child class - Car
+    # Parent class - GroundVehicle
+    pass
+
+
+class Motorcycle(GroundVehicle):
+    # Child class - Motorcycle
+    # Parent class - GroundVehicle
+    pass
